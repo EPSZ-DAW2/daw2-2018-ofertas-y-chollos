fbd1b530f1d447e5cd36ca990c03121ea1a496ce
Tomas <gootomas@gmail.com>
2018-12-17 18:31:32 +0100

    Versión inicial del Framework y configuración básica de la conexión a la BD.

---
+++ basic/config/db_defecto.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=daw2_ofertas',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+    'emulatePrepare' => true,
+];
