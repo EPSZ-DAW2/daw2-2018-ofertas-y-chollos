779ddf9173bd278bc7f16a422175eeaa91f9be65
Israel <israelcm@usal.es>
2019-01-05 11:54:05 +0100

    CRUD Usuario

---
+++ basic/views/usuarios/_search.php
@@ -0,0 +1,60 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\UsuarioSearch */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="usuario-search">
+
+    <?php $form = ActiveForm::begin([
+        'action' => ['index'],
+        'method' => 'get',
+        'options' => [
+            'data-pjax' => 1
+        ],
+    ]); ?>
+
+    <?= $form->field($model, 'id') ?>
+
+    <?= $form->field($model, 'email') ?>
+
+    <?= $form->field($model, 'password') ?>
+
+    <?= $form->field($model, 'nick') ?>
+
+    <?= $form->field($model, 'nombre') ?>
+
+    <?php // echo $form->field($model, 'apellidos') ?>
+
+    <?php // echo $form->field($model, 'fecha_nacimiento') ?>
+
+    <?php // echo $form->field($model, 'direccion') ?>
+
+    <?php // echo $form->field($model, 'zona_id') ?>
+
+    <?php // echo $form->field($model, 'fecha_registro') ?>
+
+    <?php // echo $form->field($model, 'confirmado') ?>
+
+    <?php // echo $form->field($model, 'fecha_acceso') ?>
+
+    <?php // echo $form->field($model, 'num_accesos') ?>
+
+    <?php // echo $form->field($model, 'bloqueado') ?>
+
+    <?php // echo $form->field($model, 'fecha_bloqueo') ?>
+
+    <?php // echo $form->field($model, 'notas_bloqueo') ?>
+
+    <div class="form-group">
+        <?= Html::submitButton(Yii::t('app', 'Search'), ['class' => 'btn btn-primary']) ?>
+        <?= Html::resetButton(Yii::t('app', 'Reset'), ['class' => 'btn btn-default']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
