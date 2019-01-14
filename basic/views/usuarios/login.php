af6d36ffe4911f4818aa586a5aac7cfdbf51a1e2
Israel <israelcm@usal.es>
2019-01-14 21:45:07 +0100

    FIXES: Arreglos en el formulario de login

--- basic/views/usuarios/login.php
+++ basic/views/usuarios/login.php
@@ -19,7 +19,7 @@ $this->params['breadcrumbs'][] = $this->title;
         'id' => 'login-form',
         'layout' => 'horizontal',
         'fieldConfig' => [
-            'template' => "{label}\n<div class=\"col-lg-3\">{input}</div>\n<div class=\"col-lg-8\">{error}</div>",
+            'template' => "<div class='row'>{label}\n<div class=\"col-lg-3\">{input}</div>\n<div class=\"col-lg-8\">{error}</div></div>",
             'labelOptions' => ['class' => 'col-lg-1 control-label'],
         ],
     ]); ?>
