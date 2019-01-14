3951f88bf0aa5d191549d2faffde5b5e063af121
José Antonio <joseigles@usal.es>
2019-01-14 22:00:38 +0100

    Perfil

--- basic/views/perfil/update.php
+++ basic/views/perfil/update.php
@@ -11,6 +11,7 @@ $this->title = Yii::t('app', 'Update Perfil: {nameAttribute}', [
 $this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Perfils'), 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
 $this->params['breadcrumbs'][] = Yii::t('app', 'Update');
+Yii::$app->user->id
 ?>
 <div class="perfil-update">
 
