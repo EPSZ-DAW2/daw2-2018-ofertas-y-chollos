60835a459f2cbb9448bcc75b19213483ef76f5e0
luciabg <lucia.ballestero@usal.es>
2019-01-14 14:13:59 +0100

    Update AnunciosController.php

--- basic/controllers/AnunciosController.php
+++ basic/controllers/AnunciosController.php
@@ -4,6 +4,8 @@ namespace app\controllers;
 
 use Yii;
 use app\models\Anuncio;
+use app\models\Categoria;
+use app\models\Proveedor;
 use app\models\AnuncioSearch;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
@@ -43,6 +45,16 @@ class AnunciosController extends Controller
             'dataProvider' => $dataProvider,
         ]);
     }
+     public function actionIndexPublico()
+    {
+        $searchModel = new AnuncioSearch();
+        $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
+
+        return $this->render('index_publico', [
+            'searchModel' => $searchModel,
+            'dataProvider' => $dataProvider,
+        ]);
+    }
 
     /**
      * Displays a single Anuncio model.
@@ -56,6 +68,12 @@ class AnunciosController extends Controller
             'model' => $this->findModel($id),
         ]);
     }
+     public function actionViewadmin($id)
+    {
+        return $this->render('view_admin', [
+            'model' => $this->findModel($id),
+        ]);
+    }
 
     /**
      * Creates a new Anuncio model.
@@ -66,12 +84,17 @@ class AnunciosController extends Controller
     {
         $model = new Anuncio();
 
+        
+       
+
         if ($model->load(Yii::$app->request->post()) && $model->save()) {
             return $this->redirect(['view', 'id' => $model->id]);
         }
 
         return $this->render('create', [
             'model' => $model,
+            'categorias' => $this->listarCategorias(),
+            'proveedores' => $this->listarProveedores(),
         ]);
     }
 
@@ -92,7 +115,35 @@ class AnunciosController extends Controller
 
         return $this->render('update', [
             'model' => $model,
+            'categorias' => $this->listarCategorias(),
+             'proveedores' => $this->listarProveedores(),
         ]);
+    }
+
+    public function actionVotarok($id)
+    {
+        $model = $this->findModel($id);
+        $model->votosOK = $model->votosOK+1;
+      
+        return $this->redirect(['view', 'id' => $model->id]);
+    
+        
+    }
+      public function actionVotarko($id)
+    {
+        $model = $this->findModel($id);
+        $model->votosKO++;
+       $model->save();
+        return $this->redirect(['view', 'id' => $model->id]);
+        
+    }
+    public function actionDenunciar($id)
+    {
+       $model = $this->findModel($id);
+        $model->num_denuncias++;
+       $model->save();
+        return $this->redirect(['view', 'id' => $model->id]);
+        
     }
 
     /**
@@ -123,5 +174,24 @@ class AnunciosController extends Controller
         }
 
         throw new NotFoundHttpException(Yii::t('app', 'The requested page does not exist.'));
+    }
+
+    protected function listarCategorias()
+    {
+         $listaCategorias = Categoria::find()->all();
+       $categorias = array(0=>"Ninguna");
+        foreach ($listaCategorias as $categoria) {
+           $categorias[$categoria->id]=$categoria->nombre;
+        }
+        return $categorias;
+    }
+    protected function listarProveedores()
+    {
+         $lp = Proveedor::find()->all();
+       $ps = array(0=>"Ninguno");
+        foreach ($lp as $p) {
+           $ps[$p->id]=$p->razon_social;
+        }
+        return $ps;
     }
 }
