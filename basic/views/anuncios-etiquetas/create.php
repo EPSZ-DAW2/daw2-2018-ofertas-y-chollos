1e0960f6443b1400bdd091f52069a3d9d5e59a2e
nurimateos6 <nurimateos@usal.es>
2019-01-21 20:07:38 +0100

    Update create.php

--- basic/views/anuncios-etiquetas/create.php
+++ basic/views/anuncios-etiquetas/create.php
@@ -5,7 +5,7 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model app\models\AnunciosEtiquetas */
 
-$this->title = Yii::t('app', 'Create Anuncios Etiquetas');
+$this->title = Yii::t('app', 'Crear Anuncios Etiquetas');
 $this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Anuncios Etiquetas'), 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
